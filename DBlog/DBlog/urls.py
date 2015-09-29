fd1d036773cdc67474bfa8151447b0e27e49a5e8
deanna-riddlespur <deanna.riddlespur@mexicoder.com.mx>
2015-09-29 09:34:02 -0500

    create models, views, urls, and admin

--- DBlog/DBlog/urls.py
+++ DBlog/DBlog/urls.py
@@ -1,21 +1,16 @@
-"""DBlog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    (r'^$', 'Stories.blog.views.index'),
+
+    url(
+     r'^blog/view/(?P<slug>[^\.]+).html',
+     'Stories.blog.views.view_post',
+     name='view_blog_post'),
+    url(
+     r'^blog/category/(?P<slug>[^\.]+).html',
+     'Stories.blog.views.view_category',
+     name='view_blog_category'),
 ]
